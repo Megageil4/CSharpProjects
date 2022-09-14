8b94299cc3984feac9c69c1bd4e4036a58ae8297
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2022-09-14 10:22:21 +0200

    New Projekt

---
+++ HalloVS_TLS/HalloVS_TLS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HalloVS_TLS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
