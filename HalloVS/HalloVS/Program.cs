2923b468ac25d480ec5443021f4720b672fdf1a1
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2022-09-14 10:09:37 +0200

    Erster Commit

--- HalloVS/HalloVS/Program.cs
+++ HalloVS/HalloVS/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Unser erstes Programm mit Visual Studio");
+Console.WriteLine("Unser erstes Programm mit Visual Studio!");
