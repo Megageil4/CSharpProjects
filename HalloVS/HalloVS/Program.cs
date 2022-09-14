e3a3e074447228a7a12d832eb666d2056997c78a
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2022-09-14 10:30:54 +0200

    string?

--- HalloVS/HalloVS/Program.cs
+++ HalloVS/HalloVS/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Unser erstes Programm mit Visual Studio!");
+string eingabe = Console.ReadLine();
