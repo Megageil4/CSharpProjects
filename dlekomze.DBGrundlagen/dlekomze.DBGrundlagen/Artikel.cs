5599356463e35c886b0929f7470bfb3b0366c354
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2022-09-20 15:30:08 +0200

    Angefangen Übung WH DB-Zugriff per ADO.NET

---
+++ dlekomze.DBGrundlagen/dlekomze.DBGrundlagen/Artikel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dlekomze.DBGrundlagen
+{
+	public class Artikel
+	{
+		public int ArtikelId { get; set; }
+		public string? Code { get; set; }
+		public string? Bezeichnung { get; set; }
+		public decimal Verkaufspreis { get; set; }
+		public int Wgrid { get; set; }
+		public DateTime ImAngebotSeit { get; set; }
+		public DateTime? PreisErhoetAm { get; set; }
+		public int Lagerbestand { get; set; }
+
+		public Artikel(int artikelId, string? code, string? bezeichnung, decimal verkaufspreis, int wgrid, DateTime imAngebotSeit, DateTime? preisErhoetAm, int lagerbestand)
+		{
+			ArtikelId = artikelId;
+			this.Code = code;
+			Bezeichnung = bezeichnung;
+			Verkaufspreis = verkaufspreis;
+			this.Wgrid = wgrid;
+			this.ImAngebotSeit = imAngebotSeit;
+			this.PreisErhoetAm = preisErhoetAm;
+			this.Lagerbestand = lagerbestand;
+		}
+
+		public Artikel() 
+		: this(0,"0","A",0,0,DateTime.Now,DateTime.Now,0) { }
+	
+		public Artikel(int artikelId, string? code, string? bezeichnung, decimal verkaufspreis, DateTime imAngebotSeit)
+		:this(artikelId, code, bezeichnung, verkaufspreis, 0, imAngebotSeit,null, 0) {}
+	}
+}
---
+++ dlekomze.DBGrundlagen/dlekomze.DBGrundlagen/Datenbank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace dlekomze.DBGrundlagen
+{
+	public class Datenbank
+	{
+		public Datenbank()
+		{
+
+		}
+	}
+}
