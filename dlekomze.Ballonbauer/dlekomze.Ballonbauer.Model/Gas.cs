b88447c572da7911815ab78dd5295bc93ddd2ac7
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2022-10-18 15:30:31 +0200

    Anfang Ballon

---
+++ dlekomze.Ballonbauer/dlekomze.Ballonbauer.Model/Gas.cs
@@ -0,0 +1,17 @@
+﻿namespace dlekomze.Ballonbauer.Model
+{
+	public class Gas
+	{
+		public static readonly Gas Luft = new("Luft",1.293);
+		public static const readonl Gas Helium = new("Helium", 0.1785);
+
+		public string Name { get; set; }
+		public double Dichte { get; set; }
+
+		public Gas(string name, double dichte)
+		{
+			Name = name;
+			Dichte = dichte;
+		}
+	}
+}
---
+++ dlekomze.Ballonbauer/dlekomze.Ballonbauer.Model/Kammer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dlekomze.Ballonbauer.Model
+{
+	public abstract class Kammer
+	{
+		public Gas FuellGas { get; set; }
+
+		private double _volumen;
+		public double Volumen
+		{
+			get { return _volumen; }
+		}
+
+		protected Kammer(Gas fuellGas = Gas.Luft, double volumen)
+		{
+			FuellGas = fuellGas;
+			_volumen = volumen;
+		}
+
+		protected abstract double BerechneVolumen();
+	}
+}
