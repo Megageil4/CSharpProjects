114d15299cc374aa1d3ba903b7cf67fe9912c1fa
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2022-11-22 15:30:30 +0100

    Arbeiten mit Dateisystem

---
+++ ArbeitenMitDemDateisystem/ArbeitenMitDemDateisystem/Program.cs
@@ -0,0 +1,40 @@
+﻿foreach (var drive in DriveInfo.GetDrives())
+{
+	if (drive.IsReady)
+	{
+
+		Console.WriteLine(drive.Name);
+		Console.WriteLine(drive.TotalSize);
+		Console.WriteLine(drive.AvailableFreeSpace);
+		Console.WriteLine(drive.RootDirectory);
+	}
+}
+ 
+if (!Directory.Exists(@"D:\Daten\dlekomze\myTemp"))
+{
+	Directory.CreateDirectory(@"D:\Daten\dlekomze\myTemp");
+}
+
+DirectoryInfo myTemp = new(@"D:\Daten\dlekomze\myTemp");
+if (myTemp != null)
+{ 
+	myTemp.CreateSubdirectory("Sub1");
+	myTemp.CreateSubdirectory("Sub2");
+	myTemp.CreateSubdirectory("Sub3");
+}
+
+foreach (var file in Directory.GetFiles(@"H:\Daten", "*.txt"))
+{
+	Console.WriteLine(file);
+}
+
+DirectoryInfo daten = new(@"H:\Daten");
+if (daten != null)
+{
+	foreach (var file in daten.EnumerateFiles("*.txt"))
+	{
+		Console.WriteLine(file.Name);
+	}
+}
+
+Console.ReadKey();
