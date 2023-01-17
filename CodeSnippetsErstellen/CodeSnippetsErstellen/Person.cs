f5eccdba28503733e528bfe9963bbd286cdbc01a
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2023-01-17 14:48:53 +0100

    CodeSnippit oder so

---
+++ CodeSnippetsErstellen/CodeSnippetsErstellen/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeSnippetsErstellen;
+
+public class Person
+{
+	public string Nachname { get; set; }
+
+	private string _vorname;
+
+	public string Vorname
+	{
+		get { return _vorname; }
+		set { _vorname = value; }
+	}
+
+}
